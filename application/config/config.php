a76f258d7a745582b394550a89b2eed512c3b295
DixithP <poornimavinay14@gmail.com>
2015-10-15 15:33:50 -0700

    krisazure

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['base_url']	= 'http://zion.azurewebsites.net/';
+$config['base_url']	= '';
 
 $config['index_page'] = '';
 
